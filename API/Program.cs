90c24aadec337e270cb90832415485e0177c04ff
GALLISBAIT <hoodey@hotmail.com>
2026-01-16 10:45:43 -0400

    Completion of section three

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +12,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
         opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 // Configure the HTTPS pipeline
 app.MapControllers();
